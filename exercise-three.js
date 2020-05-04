9add2f1faa106517b0ed3e97d0050253beae5d2b
kzvm <kerwin.z.vargas@gmail.com>
2020-05-04 19:13:43 -0400

    exercise-three lesson3

---
+++ exercise-three.js
@@ -0,0 +1,9 @@
+const surfboards = [['Shortboard, #1', 'Shortboard, #2'], ['Longboard, #3', 'Longboard, #4', 'Longboard, #5']];
+
+for (let i = 0; i < surfboards.length; i++) {
+
+    for (let j = 0; j < surfboards[i].length; j++) {
+        console.log(surfboards[i][j]);
+    }
+}
+
