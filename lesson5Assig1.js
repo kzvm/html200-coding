4fa44e3a10d5568bc378e315405e1a75063dfa5e
kzvm <kerwin.z.vargas@gmail.com>
2020-06-08 22:38:30 -0400

    Adding lesson5 Assig1

--- lesson5Assig1.js
+++ lesson5Assig1.js
@@ -4,56 +4,33 @@ const D = 'D';
 const B = 'B';
 
 
+let total = 0;
+
 function typeAction(action) {
     let input = prompt('Type Q, W, D or B');
 
-    //while (input !== 'Q' || input !== 'q') {
+    while (input !== 'Q' || input !== 'q') {
+        
         if (input === 'Q' || input === 'q') {
             return 'Quit Page';
 
         } else if (input === 'W' || input === 'w') {
             let input = prompt('Withdrawal Amount');
-            return 'Withdrawal';
+            total = total - Number(input);
+            alert('New Withdrawal Amount ' + total);
+            
+
         } else if (input === 'D' || input === 'd') {
             let input = prompt('Deposit Amount');
-            return 'Deposit';
+            total = total + Number(input);
+            alert('New Deposit Amount ' + total);
+           
+
         } else if (input === 'B' || input === 'b') {
-            return 'Balance';
+            alert('New Balance total ' + total);
+
         }
-    //}
+
+        input = prompt('Type Q, W, D or B');
+    }
 }
-
-
-//function typeAction(action) {
-//    let input = prompt('Type Q, W, D or B');
-
-//    switch (action) {
-//        case 'Q': {
-//            return 'Quit Page';
-//            break;
-//        }
-        
-//        case 'W': {
-//        let input = prompt('Withdrawal Amount');
-//            return 'Withdrawal';
-//            break;
-//        } 
-            
-//        case 'D': {
-//        let input = prompt('Deposit Amount');
-//            return 'Deposit';
-//            break;
-//        } 
-            
-//        case 'B': {
-//            return 'Balance';
-//            break;
-//        }
-
-//    }
-
-
-
-
-
-
